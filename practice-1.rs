1ac4643b030976b043b9d60a6db30de4743adb3c
Obumneme-1 <obumneme.madubuko@pau.edu.ng>
2025-10-08 19:50:35 +0100

    Delete practice-1.rs

--- practice-1.rs
+++
@@ -1,9 +0,0 @@
-fn mainn(){
- Println!("Welcome to COS101");
- Println!("The course learning outcome is as follows");
- Println!("1. Distinguish between computer and computer programming,/n
-           2. Develop some techniques in computer science,/n
-           3.Understand the different language of study in computer science,/n
-           4. Conversant with applications in computer science,/n
-           5. Navigate the career prospects  in computer scinece,/n
-           6. Conversant with computer programming concepts.");
