fa18302441c79fa44857b3657d4a487755d01918
Obumneme-1 <obumneme.madubuko@pau.edu.ng>
2025-10-08 19:52:48 +0100

    Add files via upload

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main() { 
+ 
+ println!(); // prints just a
+
+ Println!("hello ");//prints
+
+ println!(format {} arguments", "some");
+
+ printkn!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo");
+ 
+ }
