2f406d0b6c1c82af4518828a1e70413198fec074
unknown <obumneme.madubuko@pau.edu.ng>
2025-11-12 23:51:17 +0100

    my week 7 commit

--- week-7/practice_5/src/main.rs
+++ week-7/practice_5/src/main.rs
@@ -1,20 +1,10 @@
 fn main() {
-    
-    // Array with data type (explicit integer datatype)
-    let arr1: [i32;4] = [10,20,30,40];
-    println!("\nArray with data type");
-    println!("array is {:?}", arr1);
-    println!("array size is :{}",arr1.len());
+    let num:i32 = 5;
+    mutate_num_to_zero(num);
+    println!("The value of no is:{}",num);
 
-    // Array without data type (implicit float datatype)
-    let arr2 = [10.4,20.7,30.4,40.9,51.2,72.2];
-    println!("array is {:?}",arr2);
-    println!("array size is :{}",arr2.len());
-
-    // Array with default values that creates and
-    // initializes all its elements with a default value of -1.
-    let arr3: [i32;8] = [-1;8];
-    println!("\nArray with default values");
-    println!("array is {:?}",arr3);
-    println!("array size is :{}",arr3.len());
+fn mutate_num_to_zero(mut param_num: i32) {
+    param_num = param_num*0;
+    println!("param_num value is :{}",param_num);
+}
 }
