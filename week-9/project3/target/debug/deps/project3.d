1baf1dc0540824fc17f265c36470d70677e2ca70
unknown <obumneme.madubuko@pau.edu.ng>
2025-12-03 09:40:03 +0100

    my week 9 commit on file input and output

---
+++ week-9/project3/target/debug/deps/project3.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Documents\o.madubukoCOS101\week-9\project3\target\debug\deps\project3.d: src\main.rs
+
+C:\Users\USER\Documents\o.madubukoCOS101\week-9\project3\target\debug\deps\project3.exe: src\main.rs
+
+src\main.rs:
---
+++ week-9/project3/target/debug/project3.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Documents\o.madubukoCOS101\week-9\project3\target\debug\project3.exe: C:\Users\USER\Documents\o.madubukoCOS101\week-9\project3\src\main.rs
